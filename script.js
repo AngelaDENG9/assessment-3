87d8929fc056a1f15e96cf895bd5add9a2aa31e6
AngelaDENG9 <u3243808@uni.canberra.edu.au>
2023-10-16 16:23:57 +1100

    Final submission

---
+++ script.js
@@ -0,0 +1,47 @@
+
+
+let introSection = document.querySelector("#intro")
+// introSection.style.backgroundColor = "#ff0"
+
+introSection.className = "redback"
+
+let heading = document/createElement("h2");
+let headingText = document.createTextNode("Community Impacts");
+heading.appendChild(headingText);
+
+document.body.appendChild(heading);
+
+
+const modal = document.createElement('div');
+modal.id = 'myModal';
+
+const closeBtn = document.createElement('span');
+closeBtn.className = 'close';
+closeBtn.innerHTML = '&times;';
+closeBtn.addEventListener('click', closeModal);
+
+const modalImage = document.createElement('img');
+
+modal.appendChild(closeBtn);
+modal.appendChild(modalImage);
+document.body.appendChild(modal);
+
+function openModal(src) {
+    modal.style.display = 'block';
+    modalImage.src = src;
+}
+
+function closeModal() {
+    modal.style.display = 'none';
+}
+
+
+const images = document.querySelectorAll('.gallery-image');
+
+
+images.forEach(image => {
+    image.addEventListener('click', () => {
+        const imageUrl = image.src;
+        openModal(imageUrl);
+    });
+});
